de2136bccafd2bbcd6983992c3dda374df3c0313
David Valentiate <me@davidv.ca>
2014-10-07 13:15:10 -0400

    retries now handled internally

--- example/remote-to-remote.js
+++ example/remote-to-remote.js
@@ -1,27 +1,11 @@
 var pkgcloudContainerCopy = require('../pkgcloudContainerCopy.js');
 var remoteConfig = require('./remote-config.json');
 
-var source = pkgcloudContainerCopy.createCloudContainerSpecifer(remoteConfig.d);
-var destination = pkgcloudContainerCopy.createCloudContainerSpecifer(remoteConfig.e);
+var source = pkgcloudContainerCopy.createCloudContainerSpecifer(remoteConfig.a);
+var destination = pkgcloudContainerCopy.createCloudContainerSpecifer(remoteConfig.b);
 
-var attempt = 0;
-var attemptCopy = function () {
-	pkgcloudContainerCopy.copyContainer(source, destination)
-		.then(function (result) {
-			console.log('copy attempt:', ++attempt);
-			if (result.length) {
-				attemptCopy();
-			}
-		})
-		.catch(function (err) {
-			console.log(err);
-		})
-	;
-};
-
-attemptCopy();
-
-/* note that the rackspace API doesn return 201 indicating it has created an
- * item even when it hasn't. This is distressing. One way around it is to just
- * keep trying until it works.
- */
+pkgcloudContainerCopy.copyContainer(source, destination)
+	.done(function (result) {
+		// console.log(result.map(function (x) {return x.name;}).join('\n'));
+	})
+;
